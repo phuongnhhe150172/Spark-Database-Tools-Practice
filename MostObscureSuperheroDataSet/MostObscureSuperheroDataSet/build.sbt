980ddceb023eb868ab4a2eca393c48f1cddfc223
Nguyen Hong Phuong <imhamien2910@gmail.com>
2022-07-15 23:05:25 +0700

    Calculating the most obscure superheroes dataset

---
+++ MostObscureSuperheroDataSet/MostObscureSuperheroDataSet/build.sbt
@@ -0,0 +1,10 @@
+name := "Most-Obscure-Superhero-DataSet"
+
+version := "0.1"
+
+scalaVersion := "2.12.8"
+
+libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-core" % "3.1.3",
+  "org.apache.spark" %% "spark-sql" % "3.1.3"
+)
---
+++ MostObscureSuperheroDataSet/MostObscureSuperheroDataSet/src/main/scala/MostObscureSuperheroes.scala
@@ -0,0 +1,62 @@
+
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types.{IntegerType, StringType, StructType}
+
+//Find Superheroes with re most obscure
+object MostObscureSuperheroes {
+  case class SupperHeroNames(id: Int, name: String)
+  case class Connections(id: Int, connections: Int)
+
+  def main(args: Array[String]): Unit = {
+    //Set the log level to only print errors
+    Logger.getLogger("org").setLevel(Level.ERROR)
+
+    //Create a SparkSession using every core of the local machine
+    val spark = SparkSession
+      .builder()
+      .appName("MostObscureSuperhero")
+      .master("local[*]")
+      .getOrCreate()
+
+
+    //Create schema when reading Marvel-names.txt
+    val supperHeroNamesSchema = new StructType()
+      .add("Id", IntegerType, nullable = true)
+      .add("Name", StringType, nullable = true)
+
+
+    //Build up a hero ia, name dataset
+    import spark.implicits._
+    val supperHeroes = spark
+      .read
+      .schema(supperHeroNamesSchema)
+      .option("sep", " ")
+      .csv("data/Marvel-names.txt")
+      .as[SupperHeroNames]
+
+    supperHeroes.printSchema()
+    supperHeroes.show()
+
+    val connections = spark.sparkContext
+      .textFile("data/Marvel-graph.txt")
+      .map(line => {
+        val id = line.split(" ").head.toInt
+        val numberOfConnections = line.split(" ").tail.length
+        (id, numberOfConnections)
+      })
+      .toDF("Id", "Connections")
+      .as[Connections]
+
+    //filter the connections to find rows with only one connection
+    val one_connections = connections.filter(col("Connections") === 1)
+
+    //Join two dataset to find result
+    val result = supperHeroes.join(one_connections, supperHeroes("id") === one_connections("id"))
+
+    result.show()
+    print("The most obscure superhero is: " + result.count())
+    spark.stop()
+  }
+}
